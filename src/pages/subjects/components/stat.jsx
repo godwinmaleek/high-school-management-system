f48d1e7b364a6dfc1bbe79be755fd6da6804d399
Malik Godwin <malikgodwin2019@gmail.com>
2024-04-30 19:58:28 +0100

    create subject table component

--- src/pages/subjects/components/stat.jsx
+++ src/pages/subjects/components/stat.jsx
@@ -2,12 +2,6 @@ export default function ClassStat() {
   return (
     <div className="stats shadow grid bg-gradient-to-l from-blue-100 to-sky-50">
       <div className="stat place-items-start">
-        <div className="stat-title">Class</div>
-        <div className="stat-value text-blue-400">6</div>
-        <div className="stat-desc">Total number of classes</div>
-      </div>
-
-      <div className="stat place-items-start">
         <div className="stat-title">Subjects</div>
         <div className="stat-value text-blue-400">10</div>
         <div className="stat-desc">Total number of subjects</div>
